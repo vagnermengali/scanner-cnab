5ea58f6d17fba1219d60386cff68d6c2760585a2
vagnermengali <vagmengali2015@gmail.com>
2023-01-27 16:28:50 -0300

    feat: project completed successfully

--- transaction/models.py
+++ transaction/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+import uuid
 
 class Transaction(models.Model):
+    id = models.UUIDField(default=uuid.uuid4,primary_key=True,editable=False)
     transaction = models.CharField(max_length=1)
     date = models.CharField(max_length=8)
     value = models.DecimalField(max_digits=10, decimal_places=2)
@@ -13,5 +15,3 @@ class Transaction(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        ordering = ["id"]
